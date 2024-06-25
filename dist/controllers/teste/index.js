ce363e826b37a1bc9a7c19318d3dc466ed25f575
guilherme.araujo14 <guilherme.araujo14@hotmail.com>
2024-06-25 20:43:03 -0300

    Subindo build dos arquivos.

---
+++ dist/controllers/teste/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = PrimeiraRota;
+function PrimeiraRota(req, res) {
+    return res.status(200).send('Rota de teste funcionando de forma correta.');
+}
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
+require("dotenv/config");
+const app = (0, express_1.default)();
+const port = process.env.PORT || 9000;
+const porta = parseInt(port);
+app.use(express_1.default.json());
+app.use('/api', routes_1.default);
+app.listen(port, () => {
+    console.log('Servidor funcionando na porta ', porta);
+});
+exports.default = app;
