dc5dd28e441a8293067f1f88a15641a268893420
guilherme.araujo14 <guilherme.araujo14@hotmail.com>
2024-06-25 22:00:28 -0300

    novo ajuste

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-import PrimeiraRota from "../controllers/teste";
+import PrimeiraRota from "./controllers/teste";
 
 router.get('/minhaRota', PrimeiraRota);
 
